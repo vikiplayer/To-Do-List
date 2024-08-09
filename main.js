aa401702ce0f410d6a7ad47533c0a52088b216d2
Vicky <mvigneshwaran79@gmail.com>
2024-08-09 10:09:24 +0530

    enter task

---
+++ main.js
@@ -0,0 +1,82 @@
+let newTask = document.getElementById("add");
+let addBtn = document.getElementById("add_btn");
+let radioBtn = document.getElementById("radio_btn");
+let btn = document.getElementById("push");
+let box = document.getElementById("task-sec")
+let count = 0;
+
+// when input was active then icon as changes
+newTask.addEventListener("focus", function() {
+    addBtn.style.display = "block";
+    radioBtn.style.display = "none";
+});
+
+newTask.addEventListener("blur", function() {
+    addBtn.style.display = "none";
+    radioBtn.style.display = "block";
+});
+
+// button visible when start writing in input box
+
+newTask.addEventListener('input', function() {
+    let input = this.value;
+    let count = input.length;
+    
+    if (count > 0) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+});
+
+// for box visble
+
+if (count == 0) {
+    box.style.display = "none";
+}
+
+//adding task
+
+function addTask() {
+    if (newTask.value.length > 0) {
+        box.innerHTML += `
+        <div class="taskss">    
+            <div class="check-box">
+                <button class="check-box-btn">
+                    <i class="fa-regular fa-circle"></i>
+                </button>
+            </div>
+            <div class="task-name">
+            ${newTask.value}
+            </div>
+            <div class="important">
+                <button class="important-btn">
+                    <i class="fa-regular fa-star"></i>
+                </button>
+            </div>
+            <div class="delete">
+                <button class="delete-btn">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        </div>
+        `;  
+        newTask.value = '';
+    }
+    let tasks = document.querySelectorAll(".taskss");
+    count = count + tasks.length;
+    if (count != 0) {
+        box.style.display = "block";
+    }
+    return count;    
+}
+
+// enter key funtion
+
+btn.addEventListener("click",addTask);
+newTask.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+});
+
