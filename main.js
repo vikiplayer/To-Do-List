4ff927780bef98a73c201288e146463142436c8c
Vicky <mvigneshwaran79@gmail.com>
2024-08-12 17:50:45 +0530

    delete

--- main.js
+++ main.js
@@ -40,31 +40,34 @@ if (count == 0) {
 function addTask() {
     if (newTask.value.length > 0) {
         box.innerHTML += `
-        <div class="taskss">    
-            <div class="check-box">
-                <button class="check-box-btn">
-                    <i class="fa-regular fa-circle"></i>
-                </button>
-            </div>
-            <div class="task-name">
-            ${newTask.value}
-            </div>
-            <div class="important">
-                <button class="important-btn">
-                    <i class="fa-regular fa-star"></i>
-                </button>
-            </div>
-            <div class="delete">
-                <button class="delete-btn">
-                    <i class="fa-solid fa-trash"></i>
-                </button>
-            </div>
-        </div>
+            <div class="taskss">    
+                <div class="check-box">
+                    <button class="check-box-btn">
+                        <i class="fa-regular fa-circle"></i>
+                    </button>
+                </div>
+                <div class="task-name">
+                ${newTask.value}
+                </div>
+                <div class="important">
+                    <button class="important-btn">
+                        <i class="fa-solid fa-star iimportant"></i>
+                        <i class="fa-regular fa-star not-important"></i>
+                    </button>
+                </div>
+                <div class="delete">
+                    <button class="delete-btn">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+            </div>  
         `;  
         newTask.value = '';
+    } else {
+        alert("Please Enter a Task")
     }
     let tasks = document.querySelectorAll(".taskss");
-    count = count + tasks.length;
+    count = tasks.length;
     if (count != 0) {
         box.style.display = "block";
     }
@@ -80,3 +83,21 @@ newTask.addEventListener("keypress", function(event) {
     }
 });
 
+box.addEventListener("click", function(event) {
+    if (event.target.classList.contains("delete-btn")){
+        let task = event.target.closest(".taskss");
+        task.remove();
+        count = count - 1;
+        if (count == 0) {
+            box.style.display = "none";
+        }
+    }
+});
+
+let imp = document.getElementsByClassName("important-btn");
+let ni = document.getElementsByClassName("not-important");
+let im = document.getElementsByClassName("iimportant");
+imp.addEventListener("click", function(event){
+    im.style.display = "block";
+    ni.style.display = "none";
+})
